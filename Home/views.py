c68f95654140e7076d481619cf44f6f4d4627cd4
Raeskaa <t.mahesh@iitg.ac.in>
2021-11-18 16:21:08 +0530

    Iteration002

--- Home/views.py
+++ Home/views.py
@@ -1,79 +1,87 @@
-from django.shortcuts import render,redirect
-from .models import Complaints,Users,Events,Clubs,Boards
+from django.shortcuts import render, redirect
+from .models import Complaints, Users, Events, Clubs, Boards
 from django.contrib import messages
 from . import forms
 
 # Create your views here.
 
+
 def home(request):
     try:
-        user=Users.objects.get()
+        user = Users.objects.get()
     except Users.DoesNotExist:
         user = None
-    context={
+    context = {
         'members': user
     }
-    return render(request,'Home/home.html',context)
+    return render(request, 'Home/home.html', context)
+
 
 def senate(request):
     try:
-        user=Users.objects.get()
+        user = Users.objects.get()
     except Users.DoesNotExist:
         user = None
-    context={
+    context = {
         'members': user
     }
-    return render(request,'Home/senate.html',context)
+    return render(request, 'Home/senate.html', context)
+
 
 def cultural(request):
     try:
-        board=Boards.objects.get()
+        board = Boards.objects.get()
     except:
         board = None
-    context={
+    context = {
         'details': board
     }
-    return render(request,'Home/cultural.html',context)    
+    return render(request, 'Home/cultural.html', context)
+
 
 def technical(request):
     try:
-        board=Boards.objects.get()
+        board = Boards.objects.get()
     except:
         board = None
-    context={
+    context = {
         'details': board
     }
-    return render(request,'Home/technical.html',context) 
+    return render(request, 'Home/technical.html', context)
+
 
 def welfare(request):
     try:
-        board=Boards.objects.get()
+        board = Boards.objects.get()
     except:
         board = None
-    context={
+    context = {
         'details': board
     }
-    return render(request,'Home/welfare.html',context) 
+    return render(request, 'Home/welfare.html', context)
+
 
 def sports(request):
     try:
-        board=Boards.objects.get()
+        board = Boards.objects.get()
     except:
         board = None
-    context={
+    context = {
         'details': board
     }
-    return render(request,'Home/sports.html',context) 
+    return render(request, 'Home/sports.html', context)
+
 
 def hab(request):
     try:
-        board=Boards.objects.get()
+        board = Boards.objects.get()
     except:
         board = None
-    context={
+    context = {
         'details': board
     }
-    return render(request,'Home/hab.html',context) 
+    return render(request, 'Home/hab.html', context)
+
 
 def complaint(request):
     if request.method == 'POST':
@@ -86,8 +94,10 @@ def complaint(request):
         form = forms.ComplaintForm()
     return render(request, 'Home/complaint.html', {'form': form})
 
+
 def gallery(request):
-    return render(request,'Home/gallery.html')
+    return render(request, 'Home/gallery.html')
+
 
 def Senators(request):
     return render(request, 'Home/Senators.html')
