2b2c5af9d701fb2717f132f13a8edba59f9e9631
Raeskaa <t.mahesh@iitg.ac.in>
2022-01-28 01:31:24 +0530

    Gymkhana

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -6,14 +6,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  # /
-  path('', views.home, name='home'),
-  # TEMPORARY
-  path('signin', views.sign_in, name='signin'),
-  path('signout', views.sign_out, name='signout'),
-  path('callback', views.callback, name='callback'),
-  path('AboutDetails', views.AboutDetails, name='about-form'),
-  path('MinuteDetails', views.MinuteDetails, name='minute-form'),
-  path('GalleryDetails', views.GalleryDetails, name='gallery-form'),
-  path('SenatorDetails', views.SenatorDetails, name='senator-form'),
+    # /
+    path('', views.home, name='home'),
+    # TEMPORARY
+    path('signin', views.sign_in, name='signin'),
+    path('signout', views.sign_out, name='signout'),
+    path('callback', views.callback, name='callback'),
+    path('AboutDetails', views.AboutDetails, name='about-form'),
+    path('MinuteDetails', views.MinuteDetails, name='minute-form'),
+    path('GalleryDetails', views.GalleryDetails, name='gallery-form'),
+    path('SenatorDetails', views.SenatorDetails, name='senator-form'),
 ]
--- Authentication/views.py
+++ Authentication/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
-from django.contrib.auth import login # importing login
+from django.contrib.auth import login  # importing login
 from django.http.response import JsonResponse
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from datetime import datetime, timedelta
@@ -12,115 +12,130 @@ from django.contrib import messages
 from . import forms
 
 # <HomeViewSnippet>
-def home(request):
-  context = initialize_context(request)
 
-  return render(request, 'Authentication/login.html', context)
+
+def home(request):
+    context = initialize_context(request)
+
+    return render(request, 'Authentication/login.html', context)
 # </HomeViewSnippet>
 
 # <InitializeContextSnippet>
+
+
 def initialize_context(request):
-  context = {}
+    context = {}
 
-  # Check for any errors in the session
-  error = request.session.pop('flash_error', None)
+    # Check for any errors in the session
+    error = request.session.pop('flash_error', None)
 
-  if error != None:
-    context['errors'] = []
-    context['errors'].append(error)
+    if error != None:
+        context['errors'] = []
+        context['errors'].append(error)
 
-  # Check for user in the session
-  context['user'] = request.session.get('user', {'is_authenticated': False})
-  return context
+    # Check for user in the session
+    context['user'] = request.session.get('user', {'is_authenticated': False})
+    return context
 # </InitializeContextSnippet>
 
 # <SignInViewSnippet>
+
+
 def sign_in(request):
-  # Get the sign-in flow
-  
-  flow = get_sign_in_flow()
-  # Save the expected flow so we can use it in the callback
-  try:
-    request.session['auth_flow'] = flow
-  except Exception as e:
-    print(e)
-  # Redirect to the Azure sign-in page
-  return HttpResponseRedirect(flow['auth_uri'])
+    # Get the sign-in flow
+
+    flow = get_sign_in_flow()
+    # Save the expected flow so we can use it in the callback
+    try:
+        request.session['auth_flow'] = flow
+    except Exception as e:
+        print(e)
+    # Redirect to the Azure sign-in page
+    return HttpResponseRedirect(flow['auth_uri'])
 # </SignInViewSnippet>
 
 # <SignOutViewSnippet>
-def sign_out(request):
-  # Clear out the user and token
-  remove_user_and_token(request)
 
-  return HttpResponseRedirect(reverse('home'))
+
+def sign_out(request):
+    # Clear out the user and token
+    remove_user_and_token(request)
+
+    return HttpResponseRedirect(reverse('home'))
 # </SignOutViewSnippet>
 
 # <CallbackViewSnippet>
+
+
 def callback(request):
-  # Make the token request
-  result = get_token_from_code(request)
-  print(result)
-  #Get the user's profile
-  user = get_user(result['access_token'])
-  # Store user
-  store_user(request, user)
-  print(user)
-  print(user['displayName'])
-  print(user['mail'])
-  try:
-    user_object = User.objects.get(username = user["displayName"])
-  except:
-    user_object = User.objects.create(username = user['displayName'], email = user['mail'])
-    user_object.save()
-  #if user_object is not None: 
-    #login(request,user_object)  # we call the login function to bind a user to current session, this way they get automatically logged in to django admin website
-  #user_object.is_staff = True # we also need to set is_staff permission to true so that they have access to admin dashboard
-  #user_object.save()
-  return HttpResponseRedirect(reverse('home')) # now we just redirect to admin view, search for httpresponseredirect on django docs for more info.
+    # Make the token request
+    result = get_token_from_code(request)
+    print(result)
+    # Get the user's profile
+    user = get_user(result['access_token'])
+    # Store user
+    store_user(request, user)
+    print(user)
+    print(user['displayName'])
+    print(user['mail'])
+    try:
+        user_object = User.objects.get(username=user["displayName"])
+    except:
+        user_object = User.objects.create(
+            username=user['displayName'], email=user['mail'])
+        user_object.save()
+    # if user_object is not None:
+        # login(request,user_object)  # we call the login function to bind a user to current session, this way they get automatically logged in to django admin website
+    # user_object.is_staff = True # we also need to set is_staff permission to true so that they have access to admin dashboard
+    # user_object.save()
+    # now we just redirect to admin view, search for httpresponseredirect on django docs for more info.
+    return HttpResponseRedirect(reverse('home'))
 # </CallbackViewSnippet>
 
 
 def AboutDetails(request):
-  if request.method == 'POST':
-      form = forms.AboutForm(request.POST, request.FILES)
-      if form.is_valid():
-          form.save()
-          messages.success(request, f'Post Successful')
-          return redirect('home')
-  else:
-      form = forms.AboutForm()
-  return render(request, 'Authentication/newAddition.html', {'form': form})
+    if request.method == 'POST':
+        form = forms.AboutForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Post Successful')
+            return redirect('home')
+    else:
+        form = forms.AboutForm()
+    return render(request, 'Authentication/newAddition.html', {'form': form})
+
 
 def MinuteDetails(request):
-  if request.method == 'POST':
-      form = forms.MinuteForm(request.POST, request.FILES)
-      if form.is_valid():
-          form.save()
-          messages.success(request, f'Post Successful')
-          return redirect('home')
-  else:
-      form = forms.MinuteForm()
-  return render(request, 'Authentication/minuteAddition.html', {'form': form})
+    if request.method == 'POST':
+        form = forms.MinuteForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Post Successful')
+            return redirect('home')
+    else:
+        form = forms.MinuteForm()
+    return render(request, 'Authentication/minuteAddition.html', {'form': form})
+
 
 def GalleryDetails(request):
-  if request.method == 'POST':
-      form = forms.GalleryForm(request.POST, request.FILES)
-      if form.is_valid():
-          form.save()
-          messages.success(request, f'Post Successful')
-          return redirect('home')
-  else:
-      form = forms.GalleryForm()
-  return render(request, 'Authentication/galleryAddition.html', {'form': form})
+    if request.method == 'POST':
+        form = forms.GalleryForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Post Successful')
+            return redirect('home')
+    else:
+        form = forms.GalleryForm()
+    return render(request, 'Authentication/galleryAddition.html', {'form': form})
+
 
 def SenatorDetails(request):
-  if request.method == 'POST':
-      form = forms.SenatorForm(request.POST, request.FILES)
-      if form.is_valid():
-          form.save()
-          messages.success(request, f'Post Successful')
-          return redirect('home')
-  else:
-      form = forms.SenatorForm()
-  return render(request, 'Authentication/senatorAddition.html', {'form': form})
+    if request.method == 'POST':
+        form = forms.SenatorForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Post Successful')
+            return redirect('home')
+    else:
+        form = forms.SenatorForm()
+    return render(request, 'Authentication/senatorAddition.html', {'form': form})
