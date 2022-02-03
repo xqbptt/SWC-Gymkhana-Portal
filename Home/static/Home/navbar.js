cb0eff0e5be40624fdbf82861d5f5754e753b30f
shiv <xqbptt@gmail.com>
2022-02-03 18:33:14 +0530

    final commit

--- Home/static/Home/navbar.js
+++ Home/static/Home/navbar.js
@@ -14,6 +14,13 @@ for(ind in board_list) {
     board_id = board_list[ind];
     var boardTab = document.getElementById('board-tab-'+board_id);
     var dropdown = document.getElementById(board_id+'-dropdown')
+    if(ind<=board_list.length/2) {
+        dropdown.style.left = '-100%';
+    }
+    else {
+        dropdown.style.left = '-250%';
+        dropdown.style.marginRight = '5px';
+    }
     // console.log(dropdown.innerHTML)
     boardTab.onmouseenter = defineEnterFunction(board_id);
     boardTab.onmouseleave = defineLeaveFunction(board_id);
