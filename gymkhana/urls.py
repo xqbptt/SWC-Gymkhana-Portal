4f1747190b47a7e1dc6ffa96addae9ab123222ca
shiv <xqbptt@gmail.com>
2022-02-03 20:08:46 +0530

    final commit

--- gymkhana/urls.py
+++ gymkhana/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('gymkhana/', include('Home.urls')),
-    path('login/', include('Authentication.urls')),
+    path('stud/gymkhana/admin/', admin.site.urls),
+    path('stud/gymkhana/', include('Home.urls')),
+    path('stud/gymkhana/login/', include('Authentication.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
